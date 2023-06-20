deb609c24fd5e3f9dd2e93d0d587562221a9061f
“eescalante” <eescalante@geoautomotriz.com.bo>
2023-06-19 21:19:57 -0400

    CRUD Categoria
    Gett, Insert, Delete

--- src/app/servicios-backend/categoria/categoria.service.ts
+++ src/app/servicios-backend/categoria/categoria.service.ts
@@ -7,11 +7,16 @@ import { Observable } from 'rxjs';
 })
 export class CategoriaService {
 
-    URL_GET_CATEGORIA = "https://localhost:44301/api/Categoria"
-    URL_GET_BY_ID_CATEGORIA = "https://localhost:44301/api/Categoria/GetCategoriaById?id=6"
-    URL_ADD_CATEGORIA = "https://localhost:44301/api/Categoria/AddCategoria"
-    URL_UPDATE_CATEGORIA = "https://localhost:44301/api/Categoria/UpdateCategoria"
-    URL_DELETE_CATEGORIA = "https://localhost:44301/api/Categoria/DeleteCategoria?id=26"
+    URL_BACKEND = "https://localhost";
+    PORT_BACKEND = ":44301";
+
+    PATH_BACKEND = this.URL_BACKEND + this.PORT_BACKEND;
+
+    URL_GET_CATEGORIA = this.PATH_BACKEND + "/api/Categoria"
+    URL_GET_BY_ID_CATEGORIA = this.PATH_BACKEND + "/api/Categoria/GetCategoriaById"
+    URL_ADD_CATEGORIA = this.PATH_BACKEND + "/api/Categoria/AddCategoria"
+    URL_UPDATE_CATEGORIA = this.PATH_BACKEND + "/api/Categoria/UpdateCategoria"
+    URL_DELETE_CATEGORIA = this.PATH_BACKEND + "/api/Categoria/DeleteCategoria"
 
     constructor(private http: HttpClient) { }
 
@@ -20,6 +25,24 @@ export class CategoriaService {
         return this.http
             .get<any>(this.URL_GET_CATEGORIA,
                 { observe: 'response' })
+            .pipe();
+    }
+
+    public AddCategoria(entidad): Observable<HttpResponse<any>> {
+
+        return this.http
+            .post<any>(this.URL_ADD_CATEGORIA, entidad,
+                { observe: 'response' })
+            .pipe();
+    }
+
+    public DeleteCategoria(item): Observable<HttpResponse<any>> {
+    
+        let params = new HttpParams();
+        params = params.set('id', item.id);
+
+        return this.http
+            .post<any>(this.URL_DELETE_CATEGORIA,  "", {params: params, observe: 'response' })
             .pipe();
     }
 
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -28,17 +28,61 @@ export class Tab1Page {
                 console.log(error);
             },
             complete: () => {
-                console.log('complete - this.categoriaServices()');
+                console.log('complete - this.GetCategoria()');
             },
         });
     }
 
-    addCategoria() {
+    public addCategoria() {
         if (this.nombreCategoria.length > 0) {
-            let task = this.nombreCategoria;
-            this.listCategoria.push(task);
-            this.nombreCategoria = "";
+            var entidad = {
+                nombre : this.nombreCategoria
+            }
+            console.log(entidad)
+            this.categoriaServices.AddCategoria(entidad).subscribe({
+                next: (response: HttpResponse<any>) => {
+                    console.log(response.body)//1
+                    if(response.body == 1){
+                        alert("Se agrego la categoria con exito :)");
+                        this.GetCategoria();//Se actualize el listado
+                        this.nombreCategoria = "";
+                    }else{
+                        alert("Al agregar la categoria fallo exito :(");
+                    }
+                },
+                error: (error: any) => {
+                    console.log(error);
+                },
+                complete: () => {
+                    console.log('complete - this.addCategoria()');
+                },
+            });
         }
+    }
+
+    public updateCategoria(item){
+        console.log(item)
+    }
+
+    public deleteCategoria(item){
+        console.log(item.id)
+        this.categoriaServices.DeleteCategoria(item).subscribe({
+            next: (response: HttpResponse<any>) => {
+                console.log(response.body)//1
+                if(response.body == 1){
+                    alert("Se elimino la categoria con exito :)");
+                    this.GetCategoria();//Se actualize el listado
+                }else{
+                    alert("Al eliminar la categoria fallo exito :(");
+                }
+            },
+            error: (error: any) => {
+                console.log(error);
+            },
+            complete: () => {
+                console.log('complete - this.GetCategoria()');
+            },
+        });
     }
 
 }
