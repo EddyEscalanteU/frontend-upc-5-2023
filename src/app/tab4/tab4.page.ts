b75e1c86aea8dbdab652908a7c75f1ae4edac8c8
Israel280 <127368159+Israel280@users.noreply.github.com>
2023-06-23 10:44:57 -0400

    Hproducto

--- src/app/tab4/tab4.page.ts
+++ src/app/tab4/tab4.page.ts
@@ -1,15 +1,71 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { HttpResponse } from '@angular/common/http';
+import { HproductoService } from '../servicios-backend/hproducto/hproducto.service';
+
 
 @Component({
   selector: 'app-tab4',
-  templateUrl: './tab4.page.html',
-  styleUrls: ['./tab4.page.scss'],
+  templateUrl: 'tab4.page.html',
+  styleUrls: ['tab4.page.scss'],
 })
-export class Tab4Page implements OnInit {
+export class Tab4Page 
+{
 
-  constructor() { }
+  public listHProducto = [];
+  public idHProducto = ""
+  public cantidad = ""
+  public idProducto = ""
+  public idCarritoCompra = ""
+  public swGuardarCambios = false
 
-  ngOnInit() {
+  constructor(private hproductosService: HproductoService ) {
+    this.GetHProductos();//Se carga el listado cada vez que se abra la pag.
   }
 
+  public GetHProductos(){
+    this.hproductosService.GetHProductos().subscribe({
+        next: (response: HttpResponse <any>) => {
+            this.listHProducto = response.body;
+            //console.log(this.listProducto)
+        },
+        error: (error: any) => {
+            console.log(error);
+        },
+        complete: () => {
+            console.log('complete - this.GetProductos()');
+        },
+    });
+  }
+
+  public addHProducto() {
+    if (this.cantidad.length > 0 && this.idProducto.length > 0 && this.idCarritoCompra.length > 0) {
+      const entidad = {
+        cantidad: this.cantidad,
+        idProducto: this.idProducto,
+        idCarritoCompra: this.idCarritoCompra
+      };
+      console.log(entidad);
+  
+      this.hproductosService.AddHProducto(entidad).subscribe({
+        next: (response: HttpResponse<any>) => {
+          console.log(response.body);
+          if (response.body === 1) {
+            alert("Se agregó el HProducto con éxito :)");
+            this.GetHProductos(); // Actualizar el listado
+            this.cantidad = "";
+            this.idProducto = "";
+            this.idCarritoCompra = "";
+          } else {
+            alert("Al agregar el HProducto falló :(");
+          }
+        },
+        error: (error: any) => {
+          console.log(error);
+        },
+        complete: () => {
+          console.log('complete - this.addHProducto()');
+        }
+      });
+    }
+  }
 }
