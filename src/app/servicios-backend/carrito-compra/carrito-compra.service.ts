ff995f3d8c80bdf905a11533856ccd459f6a567f
Israel280 <127368159+Israel280@users.noreply.github.com>
2023-06-23 12:23:45 -0400

    carrito compra

--- src/app/servicios-backend/carrito-compra/carrito-compra.service.ts
+++ src/app/servicios-backend/carrito-compra/carrito-compra.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient, HttpParams, HttpResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 @Injectable({
   providedIn: 'root'
 })
 export class CarritoCompraService {
 
-  constructor() { }
+  PATH_BACKEND = environment.URL_BACKEND + environment.PORT_BACKEND;
+
+  URL_GET_CARRITOCOMPRAS = this.PATH_BACKEND + "/api/CarritoCompra"
+  URL_GET_BY_ID_CARRITOCOMPRA = this.PATH_BACKEND + "/api/CarritoCompra/GetCarritoCompraById"
+  URL_ADD_CARRITOCOMPRA = this.PATH_BACKEND + "/api/CarritoCompra/AddHCarritoCompra"
+
+
+  constructor(private http: HttpClient) { }
+
+  public GetCarritoCompras(): Observable<HttpResponse<any>> {
+
+      return this.http
+          .get<any>(this.URL_GET_CARRITOCOMPRAS,
+              { observe: 'response' })
+          .pipe();
+  }
+
+  public AddCarritoCompra(entidad): Observable<HttpResponse<any>> {
+
+      return this.http
+          .post<any>(this.URL_ADD_CARRITOCOMPRA, entidad,
+              { observe: 'response' })
+          .pipe();
+  }
 }
--- src/app/tab5/tab5.page.ts
+++ src/app/tab5/tab5.page.ts
@@ -1,15 +1,69 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { HttpResponse } from '@angular/common/http';
+import { CarritoCompraService } from '../servicios-backend/carrito-compra/carrito-compra.service';
+
 
 @Component({
   selector: 'app-tab5',
-  templateUrl: './tab5.page.html',
-  styleUrls: ['./tab5.page.scss'],
+  templateUrl: 'tab5.page.html',
+  styleUrls: ['tab5.page.scss'],
 })
-export class Tab5Page implements OnInit {
+export class Tab5Page 
+{
 
-  constructor() { }
+  public listCarritoCompra = [];
+  public id = ""
+  public idUsuario = ""
+  public fecha = ""
+ 
+ 
+  public swGuardarCambios = false
 
-  ngOnInit() {
+  constructor(private carritoCompraService: CarritoCompraService ) {
+    this.GetCarritoCompras();//Se carga el listado cada vez que se abra la pag.
   }
 
+  public GetCarritoCompras(){
+    this.carritoCompraService.GetCarritoCompras().subscribe({
+        next: (response: HttpResponse <any>) => {
+            this.listCarritoCompra = response.body;
+            //console.log(this.listProducto)
+        },
+        error: (error: any) => {
+            console.log(error);
+        },
+        complete: () => {
+            console.log('complete - this.GetCarritoCompras()');
+        },
+    });
+  }
+
+  public addCarritoCompra() {
+    if (this.idUsuario.length > 0 && this.fecha.length > 0) {
+      const entidad = {
+        idUsuario: this.idUsuario,
+        fecha: this.fecha
+      };
+  
+      this.carritoCompraService.AddCarritoCompra(entidad).subscribe({
+        next: (response: HttpResponse<any>) => {
+          console.log(response.body);
+          if (response.body === 1) {
+            alert("Se agregó el Carrito de Compra con éxito :)");
+            this.GetCarritoCompras(); // Actualizar el listado
+            this.idUsuario = "";
+            this.fecha = "";
+          } else {
+            alert("Al agregar el Carrito de Compra falló :(");
+          }
+        },
+        error: (error: any) => {
+          console.log(error);
+        },
+        complete: () => {
+          console.log('complete - this.addCarritoCompra()');
+        }
+      });
+    }
+  }
 }
