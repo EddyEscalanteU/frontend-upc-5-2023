a6f76d3310f46b0d57ff5d15ef5a8a7104a5fe2d
EddyEscalanteUstariz <78371198+EddyEscalanteUstariz@users.noreply.github.com>
2023-06-26 20:55:02 -0400

    crud-categoria COMPLETO

--- cypress/e2e/0-pruebas-integracion/categoria/crud-categoria.cy.js
+++ cypress/e2e/0-pruebas-integracion/categoria/crud-categoria.cy.js
@@ -10,17 +10,35 @@ describe('CRUD Categorias', () => {
 
     //Servicio API - GetCategoria()
     it('GetCategoria()', () => {
-        cy.get('ion-tab-button').should('be.not.visible');
+        cy.wait(1000);
+        //cy.get('ion-tab-button').should('be.not.visible');
         cy.get('ion-tab-button').eq(0).click(); // click en el TAB de Categoria
         cy.wait(1000);
         cy.get('ion-item').should('be.visible').should('not.have.length', '0'); //Verifica que exista un ion-item
     });
 
-    it('AddCategoria()', () => {
+    //Servicio API - AddCategoria(entidad)
+    it('AddCategoria(entidad)', () => {
         cy.wait(1000);
         cy.get('#nombreCategoria').type('insertar categoria cypress', { delay: 100 }).should('have.value', 'insertar categoria cypress');
         cy.wait(500);
         cy.get('#agregarCategoria').not('[disabled]').click();
     });
 
+    //Servicio API - UpdateCategoria(entidad)
+    it('UpdateCategoria(entidad)', () => {
+        cy.wait(1000);
+        cy.get('#updateCategoria').eq(0).click(); //Click al boton de Editar una categoria
+        cy.wait(1000);
+        cy.get('#nombreCategoria').invoke('val', ''); //Vaciar el campo del textfield de nombreCategoria
+        cy.get('#nombreCategoria').type('UPDATE Cypress', { delay: 100 }); //Escribir "UPDATE Cypress en el textfield de nombreCategoria"
+        cy.wait(500);
+        cy.get('#guardarCambios').not('[disabled]').click(); //Click en guardar cambios
+    });
+
+    //Servicio API - DeleteCategoria(id)
+    it('DeleteCategoria(id)', () => {
+        cy.wait(1000);
+        cy.get('#deleteCategoria').eq(0).click(); //Click al boton de Eliminar una categoria
+    });
 });
